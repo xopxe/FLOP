e31cfd3551e4ec040773d222edf367f04e3707f6
Jorge <xxopxe@gmail.com>
2015-06-04 00:21:39 -0300

    working rong setup

--- utils/boxbox.lua
+++ utils/boxbox.lua
@@ -2,6 +2,10 @@
 -- set this in __N__HERE__ label below
 local N = assert(tonumber(__N__HERE__), 'must set N value before running') 
 
+--local experiment = 'transitive'
+local experiment = 'symmetrical'
+
+
 local n = N + 1
 -- local cant_tokens=$2
 
@@ -27,7 +31,8 @@ local sched = require 'lumen.sched'
 local selector = require 'lumen.tasks.selector'
 selector.init({service='luasocket'})
 
-local notifaction_rate = 500    -- secs between notifs
+local notifaction_rate = 300    -- secs between notifs
+local token_creation_probability = 1/2 --for trw
 
 local conf = {
   name = 'node'..n, --must be unique
@@ -38,33 +43,40 @@ local conf = {
     broadcast	= 1,
     dontroute	= 0,
   },
-  send_views_timeout =  60, --5
+  send_views_timeout =  100, --5
 
-  inventory_size = 50,
+  inventory_size = 30,
 
-	--[[  
+	---[[  
   protocol = 'ron',
   max_hop_count = math.huge,
-  delay_message_emit = 20,
+  delay_message_emit = 1,
+  message_inhibition_window = 300,
   reserved_owns =50,
   max_owning_time = math.huge,
   max_ownnotif_transmits = math.huge,
-  max_notif_transmits = 100000, --10
+  max_notif_transmits = 10, --10
   max_notifid_tracked = 5000,
 	ranking_find_replaceable = 'find_replaceable_fifo',
   min_n_broadcasts = 0,
 	gamma = 0.9998,
-	p_encounter = 0.05,
+	p_encounter = 0.1,
 	min_p = 0,
 	--]]
 
+	--[[  
+  protocol = 'trw',
+  transfer_port = 0,
+  token_hold_time = 200,
+  --]]
+  
 	--[[
   protocol = 'epidemic',
   transfer_port = 0,
   max_hop_count = 10000,
 	--]]
 
-	---[[
+	--[[
   protocol = 'bsw',
   transfer_port = 0,
   max_hop_count = 10000,
@@ -78,6 +90,13 @@ local conf = {
 
 io.stdout:setvbuf('line') 
 math.randomseed(n)
+
+if conf.protocol=='trw' then
+  conf.inventory_size = math.huge
+  if math.random() < token_creation_probability then
+    conf.create_token = 'TOKEN_'..math.random(2^30)
+  end
+end
 
 for k, v in pairs(conf) do
   log('TEST', 'INFO', 'Configuration %s=%s', tostring(k), tostring(v))
@@ -114,38 +133,51 @@ local send_notification = function(target)
   rong:notificate(
     'N'..mcounter..'@'..conf.name,
     {
-      q = 'X',
+      --q = string.rep('X',20),
       target = target,
     }  
   )
 	mcounter = mcounter+1
 end
 
---[[
-if n==1 then
-	sched.run( function()
-		while true do
-			send_notification('node3')
-		  sched.sleep(notifaction_rate)
-		end
-	end)
-end
---]]
-local targets = {'node1', 'node3'}
-if n==2 then
-	sched.run( function()
-		while true do
-      for _, target in ipairs(targets) do
-        send_notification(target)
-        sched.sleep(notifaction_rate/#targets)
+local targets, sender
+if experiment == 'symmetrical' then
+  targets = {'node1', 'node3'}
+  if n==2 then
+    sender = sched.run( function()
+      while true do
+        for _, target in ipairs(targets) do
+          send_notification(target)
+          sched.sleep(notifaction_rate/#targets)
+        end
       end
-		end
-	end)
+    end)
+  end
+elseif experiment == 'transitive' then
+  targets = {'node2', 'node3'}
+  if n==1 then
+    sender = sched.run( function()
+      while true do
+        for _, target in ipairs(targets) do
+          send_notification(target)
+          sched.sleep(notifaction_rate/#targets)
+        end
+      end
+    end)
+  end
+else
+  error ('unsupported experiment: ' .. tostring(experiment))
 end
 
 sched.run(function()
+    sched.sleep(90000)
+    sender:kill()
+    sched.sleep(10000)
+end)
+
+sched.run(function()
   while true do
-    sched.sleep(conf.send_views_timeout*10)
+    sched.sleep(conf.send_views_timeout*20)
     local totals = {}
     for _, target in ipairs(targets) do
       totals[target] = 0
@@ -160,6 +192,17 @@ sched.run(function()
       s = s .. mid ..' '..total..' '
     end
     log('TEST', 'INFO', 'BUFFER CONTENT: %s', s)
+    
+    if conf.protocol=='ron' then
+      for sid, s in pairs(rong.view) do
+        log('TEST', 'INFO', 'SUBSCRIPTION P FOR %s = %f', sid, s.meta.p)
+      end
+    end
+    
+    local taskcount = 0
+    for taskd, _ in pairs (sched.tasks) do taskcount=taskcount+1 end
+    log('TEST', 'INFO', 'Lua Ram: %i, Lumen: %i tasks', 
+      collectgarbage('count')*1024, taskcount)    
   end
 end)
 
