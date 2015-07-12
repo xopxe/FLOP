631840edb22259013e6c6e3f6e7bb2177be5d2b0
xopxe <xxopxe@gmail.com>
2015-07-12 15:12:03 -0300

    updating subprojects

--- utils/boxbox.lua
+++ utils/boxbox.lua
@@ -36,6 +36,7 @@ local notifaction_rate = 300    -- secs between notifs
 local token_creation_probability = 1/2 --for trw
 
 local conf = {
+  encoder = 'lumen.lib.bencode',
   name = 'node'..n, --must be unique
   protocol_port = 8888,
   listen_on_ip = '10.1.0.'..n, 
@@ -56,7 +57,7 @@ local conf = {
   reserved_owns = 50,
   max_owning_time = math.huge,
   max_ownnotif_transmits = math.huge,
-  max_notif_transmits = 20, --10
+  max_notif_transmits = 5, --10
   max_notifid_tracked = 5000,
 	ranking_find_replaceable = 'find_replaceable_fifo',
   min_n_broadcasts = 0,
@@ -73,10 +74,15 @@ local conf = {
   reserved_owns =50,
   max_owning_time = math.huge,
   max_ownnotif_transmits = math.huge,
-  max_notif_transmits = 20, --10
+  max_notif_transmits = 10, --10
   max_notifid_tracked = 5000,
 	ranking_find_replaceable = 'find_fifo_not_on_path',
   min_n_broadcasts = 0,
+  q_decay = 0.9999,
+  q_reinf = 0.1,
+  view_skip_list = true, 
+  
+  max_path_count = 20,
 	--]]
   
 	--[[  
